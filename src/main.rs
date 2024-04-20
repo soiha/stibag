1282252da9e6ce0ca1ba233abc195bd7b865f4c2
Kalle Soiha <ksoiha@gmail.com>
2024-04-20 13:20:32 +0300

    Initial Bevy project scaffold

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use crate::stibag::StibagGamePlugin;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use bevy_rand::prelude::EntropyPlugin;
+use bevy::app::App;
+use bevy::DefaultPlugins;
+use bevy::prelude::PluginGroup;
+use bevy::prelude::ImagePlugin;
+
+mod stibag;
+
+fn main() {
+    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_plugins(WorldInspectorPlugin::new())
+        .add_plugins((StibagGamePlugin {}))
+        .run();
+}
---
+++ src/stibag/mod.rs
@@ -0,0 +1,13 @@
+﻿mod core;
+mod map;
+
+use bevy::app::App;
+use bevy::prelude::*;
+
+pub struct StibagGamePlugin {}
+
+impl Plugin for StibagGamePlugin {
+    fn build(&self, app: &mut App) {
+        
+    }
+}
